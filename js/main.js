650a8ee8a86730487c820d7d644517a170ba15d3
Jonathan Vargas <jonatan.vargas3339@gmail.com>
2022-12-05 16:05:19 -0600

    marker on map

--- js/main.js
+++ js/main.js
@@ -6,13 +6,7 @@
 // }).done(function(data) {
 // 	console.log(data);
 // });
-// // Forecast
-// $.get("http://api.openweathermap.org/data/2.5/forecast", {
-// 	APPID: OPEN_WEATHER_APPID,
-// 	q:     "San Antonio, US"
-// }).done(function(data) {
-// 	console.log(data);
-// });
+
 // ForeCast
 let lat = 29.4252;
 let long = -98.4916;
@@ -21,6 +15,7 @@ $.get("http://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ long
 	for(let i = 0; i < reports.length; i += 8) {
 		// should get 5 objects back
 		console.log(reports[i]);
+		// reports += "<h2>" + data.list + "</h2>";
 	}
 });
 
@@ -44,3 +39,10 @@ let map = new mapboxgl.Map({
 	zoom: 10,
 	center: [-98.4916, 29.4252]
 });
+
+var marker = new mapboxgl.Marker({
+	draggable: true,
+	color: "#19e813"
+})
+	.setLngLat([-98.600340, 29.591680])
+	.addTo(map);
